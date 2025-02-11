216a9e0246ca2f3a39bcd676d62ea8ea0a0ecc29
Marwane <marwane35520@gmail.com>
2025-02-11 23:40:48 +0100

    Added KMEANS/DSCAN compatibility + heatmap

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import sklearn
+import sklearn.cluster
 
 
 # Initialize database
 data_base = pd.read_excel(r"inputs\data_base.xlsx")
+
+print(data_base.info())
 
 # Plot the locations based on Longitude as x axis and Latitude as y axis
 plt.figure(figsize=(10, 6))
@@ -41,7 +44,7 @@ plt.ylabel("Latitude")
 plt.show()
 """
 
-# Selects 
+# Selects 200 to 300 deliveries from the dataset
 sample = data_base.sample(n=np.random.randint(200, 301), random_state=42)
 
 #
@@ -52,17 +55,31 @@ scaler = sklearn.preprocessing.StandardScaler()
 sample[['Latitude_scaled', 'Longitude_scaled']] = scaler.fit_transform(sample[['GPS - Latitude', 'GPS - Longitude']])
 
 kmeans = sklearn.cluster.KMeans(n_clusters=num_clusters, random_state=42)
+dbscan = sklearn.cluster.DBSCAN(eps=0.5)
 
-sample['Cluster'] = kmeans.fit_predict(sample[['Latitude_scaled', 'Longitude_scaled']])
+print(kmeans.cluster_centers_)
 
+sample['Cluster_kmeans'] = kmeans.fit_predict(sample[['Latitude_scaled', 'Longitude_scaled']])
+sample['Cluster_DBSCAN'] = kmeans.fit_predict(sample[['Latitude_scaled', 'Longitude_scaled']])
+
+# Plot clusters using kmeans
 plt.figure(figsize=(10, 6))
-
-sns.scatterplot(x=sample['GPS - Longitude'], y=sample['GPS - Latitude'], hue=sample['Cluster'], palette='viridis')
-
-plt.title("Clustered Delivery Routes")
+sns.scatterplot(x=sample['GPS - Longitude'], y=sample['GPS - Latitude'], hue=sample['Cluster_kmeans'], palette='viridis')
+plt.title("Clustered Delivery Routes using KMEANS")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 plt.legend(title="Cluster")
 plt.show()
+
+# Plot clusters using DBSCAN
+"""
+plt.figure(figsize=(10, 6))
+sns.scatterplot(x=sample['GPS - Longitude'], y=sample['GPS - Latitude'], hue=sample['Cluster_DBSCAN'], palette='viridis')
+plt.title("Clustered Delivery Routes using DBSCAN")
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.legend(title="Cluster")
+plt.show()
+"""
 
 #daily_sample.to_csv("clustered_deliveries.csv", index=False)
