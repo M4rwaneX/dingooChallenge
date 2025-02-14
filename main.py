d5219b8e8a5fbc284951611a40c69eeb24948e41
EL HANI Marwane <marwane.el-hani@imt-atlantique.net>
2025-02-14 18:13:31 +0100

    plot new stat

--- main.py
+++ main.py
@@ -100,5 +100,6 @@ plt.show()
 
 stats = statistics_clusters.statistics_clusters(database)
 #stats.plot_silhouette_scores(100)
-stats.plt_silhouette_score_differents_k()
+#stats.plt_silhouette_score_differents_k()
+stats.plt_max_silhouette(30)
 
