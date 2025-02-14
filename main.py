04aa38b3a1bc386d98519a79b105d6fa16d2a343
EL HANI Marwane <marwane.el-hani@imt-atlantique.net>
2025-02-14 11:10:29 +0100

    Added silhouette score

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import sklearn
-import sklearn.cluster
-import sklearn.metrics
+
 
 def max_mean_distance_centroids(kmeans,centroids,sample):
     max_distances = pd.Series(dtype=float)
@@ -90,7 +89,7 @@ plt.show()
 """
 
 # Selects 200 to 300 deliveries from the dataset
-sample = data_base.sample(n=np.random.randint(200, 301), random_state=42)
+sample = data_base.sample(n=250,random_state=42)
 
 # We want about 20 to 30 locations for each clusters
 num_clusters = len(sample) // 25
@@ -113,6 +112,7 @@ plt.show()
 """
 
 # Scaling Latitude and Longitude
+
 scaler = sklearn.preprocessing.StandardScaler()
 sample[['Latitude_scaled', 'Longitude_scaled']] = scaler.fit_transform(sample[['GPS - Latitude', 'GPS - Longitude']])
 kmeans = sklearn.cluster.KMeans(n_clusters=num_clusters, random_state=42)
@@ -125,12 +125,9 @@ sample['Cluster_DBSCAN'] = kmeans.fit_predict(sample[['Latitude_scaled', 'Longit
 
 # Plot clusters using kmeans
 """
-
+max_distances_centroid,mean_distances_centroid = max_mean_distance_centroids(kmeans,kmeans.cluster_centers_,sample)
 plt.figure(figsize=(10, 6))
 sns.scatterplot(x=sample['Longitude_scaled'], y=sample['Latitude_scaled'], hue=sample['Cluster_kmeans'], palette='viridis')
-circle = plt.Circle(kmeans.cluster_centers_[0], max_distances[0], color='gray', fill=False, linestyle="dashed")
-plt.gca().add_patch(circle)
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], c='red', marker='x', s=200, label="Centroïdes")
 plt.title("Clustered Delivery Routes using KMEANS")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
@@ -139,10 +136,8 @@ plt.show()
 """
 
 # Plot n cluster using kmeans with circles based centroids
-"""
+
 max_distances_centroid,mean_distances_centroid = max_mean_distance_centroids(kmeans,kmeans.cluster_centers_,sample)
-barycenters = compute_barycenters(kmeans,kmeans.cluster_centers_,sample)
-max_distances_barycenter,mean_distances_barycenter = max_mean_distance_barycenter(kmeans,barycenters,sample)
 plt.figure(figsize=(10, 6))
 for n in range(4,7):
     points_cluster = sample.loc[kmeans.labels_ == n,
@@ -152,7 +147,7 @@ for n in range(4,7):
                     alpha=0.5)
 
     circle = plt.Circle(kmeans.cluster_centers_[n],
-                        max_distances_centroids[n],
+                        max_distances_centroid[n],
                         color='gray',
                         fill=False,
                         linestyle="dashed")
@@ -164,12 +159,17 @@ for n in range(4,7):
                 s=200,
                 label="Centroïdes " + str(n))
 
+print(sklearn.metrics.silhouette_score(
+    sample[['Latitude_scaled', 'Longitude_scaled']],
+    sample['Cluster_kmeans']))
+
 plt.title("Clusters")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 plt.legend(title="Cluster")
 plt.show()
-"""
+
+
 
 # Plot n clusters using kmeans with circles based on barycenters
 
@@ -240,9 +240,9 @@ plt.legend(title="Cluster")
 plt.show()
 """
 
-
+"""
 barycenters = compute_barycenters(kmeans,kmeans.cluster_centers_,sample)
-kmeans_barycenters = sklearn.cluster.KMeans(n_clusters=num_clusters,init=barycenters)
+kmeans_barycenters = sklearn.cluster.KMeans(n_clusters=num_clusters,init=barycenters,n_init=1)
 sample['Cluster_kmeans_barycenters'] = kmeans_barycenters.fit_predict(sample[['Latitude_scaled', 'Longitude_scaled']])
 barycenters = compute_barycenters(kmeans_barycenters,kmeans_barycenters.cluster_centers_,sample)
 max_distances_barycenter,mean_distances_barycenter = max_mean_distance_barycenter(kmeans_barycenters,barycenters,sample)
@@ -266,14 +266,18 @@ for n in range(6):
                 marker='x',
                 s=200,
                 label="Barycenter " + str(n))
-
-print(sklearn.metrics.silhouette_score(sample))
-
+                
 plt.title("Clusters")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 plt.legend(title="Cluster")
 plt.show()
+"""
+#print(sklearn.metrics.silhouette_score(sample[['Latitude_scaled', 'Longitude_scaled']],sample['Cluster_kmeans']))
+#print(sklearn.metrics.silhouette_score(sample[['Latitude_scaled', 'Longitude_scaled']],sample['Cluster_kmeans_barycenters']))
+
+
+
 
 
 # Plot clusters using DBSCAN
